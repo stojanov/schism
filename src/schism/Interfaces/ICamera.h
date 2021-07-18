93668b8de7b282204c7ba2c30a90fbd54d86c615
Filip Vojnovski <c00l4l1f3@gmail.com>
2021-07-18 22:21:28 +0200

    Added public access modifier which was forgotten

--- src/schism/Interfaces/ICamera.h
+++ src/schism/Interfaces/ICamera.h
@@ -6,6 +6,7 @@ namespace Schism
 {
 	class ICamera
 	{
+	public:
 		virtual const glm::mat4& GetProjectionMatrix() const = 0;
 		virtual const glm::mat4& GetViewMatrix() const = 0;
 		virtual const glm::mat4& GetViewProjectionMatrix() const = 0;
