b4fcaefc66986c96782ba625847886d29a17f03d
Stojanov <stojanovangel@outlook.com>
2021-07-19 20:10:47 +0200

    Add a centralized render api

---
+++ src/schism/Renderer/RenderAPI.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdint>
+
+#include "vec4.hpp"
+
+namespace Schism::Renderer
+{
+	class API
+	{
+	public:
+		static void Init();
+		static void Shutdown();
+
+		static void SetClearColor(const glm::vec4& color);
+		static void SetViewport(uint32_t width, uint32_t height);
+		static void Clear();
+
+		// TODO: Add drawing functionality 
+	};
+}
