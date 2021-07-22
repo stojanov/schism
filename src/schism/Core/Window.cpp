9b91df3bb1498b8f2a53b2b14c31a88b872c4d54
Filip Vojnovski <c00l4l1f3@gmail.com>
2021-07-22 04:50:50 +0200

    Reverted changes that were in incorrect branch.

--- src/schism/Core/Window.cpp
+++ src/schism/Core/Window.cpp
@@ -66,12 +66,6 @@ namespace Schism::Core
 	{
 		m_Data.OnEvent = callback;
 	}
-
-	void Window::Clear() const
-	{
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-	}
 	
 	void Window::Swap() const
 	{
--- src/schism/Sandbox/SampleScene.cpp
+++ src/schism/Sandbox/SampleScene.cpp
@@ -48,8 +48,6 @@ void SampleScene::OnSystemEvent(Event& e)
 
 void SampleScene::OnUpdate(Timestep ts)
 {
-	auto& transform = m_Registry.get<Components::Transform2D>(m_Ship1);
-	transform.position.x += 0.1;
 }
 
 void SampleScene::OnDraw()
